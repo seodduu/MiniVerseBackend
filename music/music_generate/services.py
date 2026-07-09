d0a5c132b95cb330d29877b92bb47f06163730e6
seodduu <tjengus9@gmail.com>
2026-07-09 15:43:55 +0900

    baseline snapshot before AI generation notification feature (refreshed)

--- music/music_generate/services.py
+++ music/music_generate/services.py
@@ -10,7 +10,7 @@ import requests
 import time
 from typing import Dict, Optional
 
-from langchain_community.chat_models import ChatOllama
+from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
